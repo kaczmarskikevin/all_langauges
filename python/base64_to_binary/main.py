31fee176275e519dae30df1dc254e4134b1cbfc8
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2025-01-06 13:23:50 -0500

    moved file copy

--- python/base64_to_binary/main.py
+++ python/base64_to_binary/main.py
@@ -1,8 +1,8 @@
 import base64
 import sys
 
-print(sys.argv[1])
-decoded = base64.decodebytes(sys.argv[1])
+print(sys.argv[0])
+decoded = base64.decodebytes(sys.argv[0])
 
 print(decoded)
 print(len(decoded)*8)
