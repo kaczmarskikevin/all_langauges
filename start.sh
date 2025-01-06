423d05e338a5db0179b09e3617d4107a8593b41e
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2025-01-06 13:30:53 -0500

    testing delete

--- start.sh
+++ start.sh
@@ -4,4 +4,8 @@ cd $1
 docker build -t kaczmarskikevin/$1 .
 cd $2
 docker build -t kaczmarskikevin/$1-$2 .
-echo $(docker run kaczmarskikevin/$1-$2)
+
+ARGS=`$@/$1`
+$ARGS=`$ARGS/$2`
+
+echo $(docker run kaczmarskikevin/$1-$2 $ARGS)
