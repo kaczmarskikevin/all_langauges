bbef4853cbc40a0b9720480fd3d5b58d4d12a358
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2025-01-06 17:10:36 -0500

    make doesn't need arguments

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t kaczmarskikevin/$1 /$1/
+docker build -t kaczmarskikevin/$1 ./$1/
 cd $2
-docker build -t kaczmarskikevin/$1-$2 /$1-$2/
+docker build -t kaczmarskikevin/$1-$2 ./$1/$2/
 
 LANG="$1"
 PROG="$2"
