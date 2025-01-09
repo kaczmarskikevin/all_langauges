17311f9098184d52648ff47f69acbcde7df926e8
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2025-01-09 15:42:11 -0500

    c

---
+++ cobol/hello_world/hello_world.cbl
@@ -0,0 +1,6 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-READERS.
+      *simple cobol program.
+       PROCEDURE DIVISION.
+           DISPLAY 'Hello, World!'.
+           STOP RUN.
