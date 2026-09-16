74f0a133dc9195784fb996ba96e17a878ff42b1d
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2026-09-16 17:40:48 -0400

    tesT

--- c/pointer_fun/src/main.c
+++ c/pointer_fun/src/main.c
@@ -4,26 +4,26 @@
 int main(void) {
     int number = 10;
 
-    printf("int number address is %p!\n", &number);
-    printf("int number value is %p!\n", number);
-    printf("int number value is %d!\n", number);
+    printf("int number address is %p\n", &number);
+    printf("int number value is %p\n", number);
+    printf("int number value is %d\n", number);
 
     int* number_p = &number;
 
-    printf("&number_p is %p!\n", &number_p);
-    printf("*number_p is %p!\n", *number_p);
-    printf("number_p is %p!\n", number_p);
-    printf("&number_p is %d!\n", &number_p);
-    printf("*number_p is %d!\n", *number_p);
-    printf("number_p is %d!\n", number_p);
+    printf("&number_p is %p\n", &number_p);
+    printf("*number_p is %p\n", *number_p);
+    printf("number_p is %p\n", number_p);
+    printf("&number_p is %d\n", &number_p);
+    printf("*number_p is %d\n", *number_p);
+    printf("number_p is %d\n", number_p);
 
     hello_t hello = {"hello\0"};
     
-    printf("&hello.phrase is %p!\n", &hello.phrase);
-    printf("*hello.phrase is %p!\n", *hello.phrase);
-    printf("*hello.phrase is %c!\n", *hello.phrase);
-    printf("hello.phrase is %p!\n", hello.phrase);
-    printf("hello.phrase is %s!\n", hello.phrase);
-    printf("hello.phrase is %c!\n", hello.phrase);
+    printf("&hello.phrase is %p\n", &hello.phrase);
+    printf("*hello.phrase is %p\n", *hello.phrase);
+    printf("*hello.phrase is %c\n", *hello.phrase);
+    printf("hello.phrase is %p\n", hello.phrase);
+    printf("hello.phrase is %s\n", hello.phrase);
+    printf("hello.phrase is %c\n", hello.phrase);
     
 }
