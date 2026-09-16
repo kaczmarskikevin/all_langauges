50eacf9a08223788c764ddbc8f3c99b79770c5ca
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2026-09-16 16:16:26 -0400

    test

---
+++ c/pointer_fun/src/main.c
@@ -0,0 +1,11 @@
+int main(void) {
+
+    int number = 10;
+
+    printf("number is %d!\n", number);
+    printf("number is %f!\n", number);
+    printf("number is %c!\n", number);
+    printf("number is %s!\n", number);
+    printf("number is %x!\n", number);
+    printf("number is %p!\n", number);
+}
