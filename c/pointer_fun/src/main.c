3e6fd5bd9052828ccfd3ad8f57f0074e5227d7fc
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2026-09-16 17:03:48 -0400

    tesT

--- c/pointer_fun/src/main.c
+++ c/pointer_fun/src/main.c
@@ -2,10 +2,13 @@ int main(void) {
 
     int number = 10;
 
-    printf("number is %d!\n", number);
+    printf("int number address is %p!\n", &number);
+    printf("int number value is %p!\n", number);
+    printf("int number value is %d!\n", number);
 
     int* number_p = &number;
 
-    printf("number_p is %p!\n", number_p);
-    printf("number_p is %d!\n", *number_p);
+    printf("pointer number_p address is %p!\n", &number_p);
+    printf("pointer number_p value is %p!\n", *number_p);
+    printf("pointer number_p value is %d!\n", number_p);
 }
